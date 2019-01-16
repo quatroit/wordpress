60aba48d00cf51d99eefd33ae4a1824085faff2b
quatroit <46746827+quatroit@users.noreply.github.com>
2019-01-16 13:13:08 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@ function remove_styles () {
 }
 add_action ('wp_print_styles','remove_styles',100);
 
-/*подключение .js в футере*/
+/* подключение .js в футере */
 function footer_enqueue_scripts(){
     remove_action('wp_head','wp_print_scripts');
     remove_action('wp_head','wp_print_head_scripts',9);
@@ -16,3 +16,57 @@ function footer_enqueue_scripts(){
     add_action('wp_footer','wp_print_head_scripts',5);
 }
 add_action('after_setup_theme','footer_enqueue_scripts');
+
+/* увеличение загружаемого файла */
+add_filter( 'upload_size_limit', 'PBP_increase_upload' );
+function PBP_increase_upload( $bytes )
+{
+ return 15728640;
+}
+
+/*перевод url в нижний регистр*/
+if(!is_admin()){
+	add_action( 'init', 'vijay_case_sensitive_url' );
+}
+
+function vijay_case_sensitive_url(){
+
+	$landing_url = $_SERVER['REQUEST_URI'];
+
+	if(preg_match('/[\.]/', $landing_url)){
+		return;
+	}
+
+	if(preg_match('/[A-Z]/', $landing_url)){
+
+		$new_url = strtolower($landing_url);
+		header("HTTP/1.1 301 Moved Permanently");
+		header("Location: " . $new_url);
+		exit(0);
+	}
+}
+
+/*выводим в rss (фиды) ленту пост тайпы('catalog'-это название пост тайпа) )*/
+
+add_filter('pre_get_posts', 'add_new_post_types_to_feed');
+function add_new_post_types_to_feed( $query ) {
+    // Выходим если это запрос не фидов
+    if( ! $query->is_feed || ! $query->is_main_query() )
+        return;
+
+    $query->set( 'post_type', array('post', 'catalog') );
+}
+
+/*кол-во записей в рсс*/
+add_filter( 'pre_get_posts', 'how_many_posts_display_in_feed' );
+function how_many_posts_display_in_feed($query) {
+    if( ! $query->is_feed || ! $query->is_main_query() )
+        return;
+
+    // этот вариант не работает
+    // $query->set( 'posts_per_page', 11 );
+
+    // Сколько записей показывать
+    $n = 15;
+    add_filter( 'post_limits', create_function('', "return 'LIMIT $n';") );
+}
